403cff67b00632be77bdaa31a6aff742bdff5205
清云游霄 <55185354@qq.com>
2025-12-01 21:59:13 +0800

    add WebConfig.java

--- src/main/java/com/qingyunyouxiao/sbsn/controllers/UserController.java
+++ src/main/java/com/qingyunyouxiao/sbsn/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.qingyunyouxiao.sbsn.controllers;
 
 import java.util.List;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/qingyunyouxiao/sbsn/services/UserService.java
+++ src/main/java/com/qingyunyouxiao/sbsn/services/UserService.java
@@ -1,6 +1,5 @@
 package com.qingyunyouxiao.sbsn.services;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
